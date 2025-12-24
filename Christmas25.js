77b4550f8385363c0fd1b52ca3a049d54e53a7d3
phatdarin <patdarin.npj@gmail.com>
2025-12-24 20:48:08 +0700

    Add files via upload
    Reindeer looks like everything but Reindeer. Thank you very much.

---
+++ Christmas25.js
@@ -0,0 +1,42 @@
+let snowInterval = null;
+const toggle = document.getElementById("toggle");
+
+document.body.classList.add("night");
+snowInterval = setInterval(makeSnow, 40);
+
+toggle.addEventListener("change", () => {
+    document.body.classList.toggle("day", toggle.checked);
+    document.body.classList.toggle("night", !toggle.checked);
+
+    if (!toggle.checked) {
+        if (!snowInterval) {
+            snowInterval = setInterval(makeSnow, 40);
+        }
+    } else {
+    
+        clearInterval(snowInterval);
+        snowInterval = null;
+
+        document.querySelectorAll(".snow").forEach(s => s.remove());
+    }
+});
+
+function makeSnow() {
+    const snow = document.createElement("div");
+    const size = Math.random() * 4.5 + 3.5;
+
+    snow.className = "snow";
+    snow.style.width = size + "px";
+    snow.style.height = size + "px";
+    snow.style.left = Math.random() * window.innerWidth + "px";
+    snow.style.opacity = size / 8;
+
+    if (size < 7) snow.style.zIndex = -5;
+
+    snow.style.animationDuration = Math.random() * 3 + 2 + "s";
+    document.body.appendChild(snow);
+
+    setTimeout(() => snow.remove(), 5000);
+}
+
+document.body.classList.add("night");
